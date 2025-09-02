4ddd875a5098ac9b9200c0aa84091f9999e4e153
shinataputra <71615861+shinataputra@users.noreply.github.com>
2025-09-02 13:31:00 +0700

    first commit

---
+++ Variable/variable.py
@@ -0,0 +1,15 @@
+namadepan = "Asep"
+namabelakang = "Sopyan"
+alamat = "Mastrip"
+hobi = "Sepak Bola"
+
+mat = 90
+agama = 100
+pkn = 80
+
+ratarata = (mat + agama + pkn) / 3
+
+
+print("Nama saya adalah " + namadepan +" "+namabelakang + "dan saya tinggal di " + alamat + " dan hobisaya adalah " + hobi)
+
+#  lanjutkan dengan varibale yang belum digunakan
